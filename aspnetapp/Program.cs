b237569b7a42a33045a8777fa5e929f593ebfd62
barnett <zengqg@goodrain.com>
2018-07-27 17:10:45 +0800

    change listen port

--- aspnetapp/Program.cs
+++ aspnetapp/Program.cs
@@ -8,17 +8,18 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace aspnetapp
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
+namespace aspnetapp {
+    public class Program {
+        public static void Main (string[] args) {
+            string port = "5000";
+            if (Environment.GetEnvironmentVariable ("PORT") != "") {
+                port = Environment.GetEnvironmentVariable ("PORT");
+            }
+            CreateWebHostBuilder (port, args).Build ().Run ();
         }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder (string port, string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseUrls ($"http://0.0.0.0:{port}")
+            .UseStartup<Startup> ();
     }
 }
